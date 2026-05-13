cc1c1db133159581069ae828920526022d822d9b
Patrik <patrik.kaleja2002@gmail.com>
2026-05-14 00:14:59 +0200

    Remove logout, extend session to 30 days

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET || "webagent-secret-xyz",
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
 }));
 
 app.use(passport.initialize());
